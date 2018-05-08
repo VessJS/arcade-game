0c996a57330be505b16d8c588c6becc85ca80c23
Marcin Andrzejewski <andrzejewski.m4@gmail.com>
2018-05-08 17:31:42 +0200

    keyboard controls added

--- js/app.js
+++ js/app.js
@@ -35,33 +35,75 @@ class Base {
     }
 }
 
-class Player extends Base {
-    constructor() {
-        super();
-        this.sprite = 'images/char-horn-girl.png';
-    }
-
-    update() {
-
-    }
-
-    render() {
-
-    }
-
-    handleInput() {
-
-    }
-
-}
-
 class Enemy extends Base {
     constructor() {
         super();
+        this.x = this.getRndInteger(-100, 0) * 20;
+        this.y = this.getRndInteger(1, 3) * 73;
         this.sprite = 'images/enemy-bug.png';
+    }
+
+    update(dt) {
+        this.x += 300 * dt;
+        if (this.x > 500) {
+            this.x = this.getRndInteger(-50, 0) * 10;
+            this.y = this.getRndInteger(70, 240);
+        }
+    }
+
+    getPosition() {
+        return [this.ex, this.ey];
+    }
+}
+
+class Player extends Base {
+    constructor() {
+        super();
+        this.x = 200;
+        this.y = 290;
+        this.sprite = 'images/char-horn-girl.png';
+
     }
     update() {
 
+    }
+
+    handleInput(key = "") {
+        switch (key) {
+            case "left":
+                this.x -= 100;
+                break;
+            case "right":
+                this.x += 100;
+                break;
+            case "up":
+                this.y -= 80;
+                break;
+            case "down":
+                this.y += 80;
+                break;
+            default:
+                return [this.x, this.y];
+        }
+        if (this.y < 0) {
+            this.reset();
+        }
+        if (this.y > 380 || this.x < 0 || this.x > 400) {
+            this.reset();
+        }
+    }
+    checkCollisions(enemies) {
+        enemies.forEach(enemy => {
+           let [x, y] = enemy.getPosition();
+
+            if (x > this.x && y > this.y) {
+                this.reset();
+            }
+        });
+    }
+    reset() {
+        this.x = 200;
+        this.y = 290;
     }
 }
 
@@ -69,8 +111,8 @@ class Enemy extends Base {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 let player = new Player();
-letallEnemies = [new Enemy(), new Enemy(), new Enemy()];
-
+let allEnemies = [new Enemy(), new Enemy(), new Enemy()];
+player.checkCollisions(allEnemies);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
